b77ee234c927257cb354647db32a4ae563fb8045
xieguanling <xieguanling@huya.com>
2019-04-18 15:49:19 +0800

    @EnableJpaAuditing 没有这个注解不会更新记录时间

--- src/main/java/com/ws/BootApplication.java
+++ src/main/java/com/ws/BootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableAutoConfiguration
 @EnableWebMvc
+@EnableJpaAuditing
 @ComponentScan( basePackageClasses = {BootApplication.class})
 public class BootApplication {
 
